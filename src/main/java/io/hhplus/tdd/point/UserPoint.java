936ed4a2b6aa5b044d040b9de8aa65208da114e8
omega <soh95@naver.com>
2025-03-24 18:45:53 +0900

    feat: 유저 포인트 증가, 감소 기능 추가

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -6,7 +6,37 @@ public record UserPoint(
         long updateMillis
 ) {
 
+    public static final long MAX_POINT = 100L;
+
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
+    }
+
+    public UserPoint plusPoint(long amount) {
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than 0");
+        }
+
+        long addedPoint = point + amount;
+
+        if (addedPoint > MAX_POINT) {
+            throw new IllegalStateException("not enough point");
+        }
+        return new UserPoint(id, addedPoint, System.currentTimeMillis());
+    }
+
+    public UserPoint minusPoint(long amount) {
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than 0");
+        }
+
+        long remainingPoint = point - amount;
+
+        if (remainingPoint < 0) {
+            throw new IllegalStateException("not enough point");
+        }
+        return new UserPoint(id, remainingPoint, System.currentTimeMillis());
     }
 }
